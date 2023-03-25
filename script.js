5da300a73b01b7f04c293e536b8939a36f758a05
Hritik Sharma <72138429+hritik2002@users.noreply.github.com>
2023-03-25 20:47:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -30,3 +30,41 @@ async function connectToSearchResults() {
     }
   }, 2000);
 }
+
+
+
+
+// follows everyone
+ setInterval(() => {
+        console.log("NEW PAGE");
+        async function connectToSearchResults() {
+  const container = document.querySelector('.reusable-search__entity-result-list');
+  if (!container) return;
+
+  const resultItems = container.children;
+  const itemKeys = Object.keys(resultItems);
+  let i = 0, len = itemKeys.length;
+  const intervalId = setInterval(async () => {
+    window.scrollTo(0, document.body.scrollHeight);
+    if (i >= len) {
+      console.log("Clearing setInterval...");
+      clearInterval(intervalId);
+      const next = document.querySelector(".artdeco-pagination__button.artdeco-pagination__button--next.artdeco-button.artdeco-button--muted.artdeco-button--icon-right.artdeco-button--1.artdeco-button--tertiary.ember-view")
+     
+      if(next) next.click();
+      return;
+    }
+    const itemKey = itemKeys[i++];
+    if (!isNaN(itemKey)) {
+      const connectBtn = resultItems[itemKey].querySelector('button');
+      console.log(connectBtn.innerText);
+      if (connectBtn.innerText === 'Follow') {
+        
+          connectBtn.click();
+        
+      }
+    }
+  }, 100);
+}
+        connectToSearchResults();
+    }, 2000)
